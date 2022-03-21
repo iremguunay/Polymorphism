629a8aba74150c2f5a43993acb6e2c046cec8f88
Irem Gunay <iremguunay@gmail.com>
2022-03-22 00:11:56 +0300

    Added another polymorphism example

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 
+    public static void addRandomNumber(ArrayList<Integer> arrList) {
+        int originalSize = arrList.size();
+        Random random = new Random();
+        while (originalSize + 1 != arrList.size()) {
+            int n = random.nextInt(1000);
+            arrList.add(n);
+        }
+    }
+
     public static void main(String[] args) {
 
+        //Example 2
+        OddArrayList oddListy = new OddArrayList(7);
+        oddListy.add(1);
+        oddListy.add(2);
+        addRandomNumber(oddListy);
+        System.out.println(oddListy);
+
+        ArrayList<Integer> listy = new ArrayList<>();
+        listy.add(1);
+        listy.add(2);
+        addRandomNumber(listy);
+        System.out.println(listy);
+
+        //Example1
         OddArrayList oddList = new OddArrayList(1,2,3,4,7,9,-13);
-        System.out.println(oddList);
+        //System.out.println(oddList);
 
         ArrayList<Integer> list = new ArrayList<>();
 
@@ -17,7 +41,7 @@ public class Main {
         list.add(9);
         list.add(-13);
 
-        System.out.println(list);
+        //System.out.println(list);
 
     }
 }
